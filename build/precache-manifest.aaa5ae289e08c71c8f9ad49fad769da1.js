f28e51e821fe9083ff5cc0ea138a4699d7ccbeb5
deilsonmartins <deilsonengeletrica@gmail.com>
2020-07-06 23:06:19 -0300

    build

---
+++ build/precache-manifest.aaa5ae289e08c71c8f9ad49fad769da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9937d5a0162e46415088f0b4804af22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88673dd8d735b8bd0c85",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "88673dd8d735b8bd0c85",
+    "url": "/static/js/2.f36981ed.chunk.js"
+  },
+  {
+    "revision": "f72314db4e3a8c133378",
+    "url": "/static/js/main.fc3c01e2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2d5ba1aa7d474f98a38e88041491c4ca",
+    "url": "/static/media/pikachu.2d5ba1aa.svg"
+  }
+]);
